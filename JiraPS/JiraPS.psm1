0bbacd6b7720d5bbea3dda7ffe95618105c74f77
Oliver Lipkau <oliver@lipkau.net>
2021-03-22 00:21:40 +0100

    Prepared module for major upgrade

--- JiraPS/JiraPS.psm1
+++ JiraPS/JiraPS.psm1
@@ -1,4 +1,4 @@
-﻿#region Dependencies
+#region Dependencies
 # Load the ConfluencePS namespace from C#
 if (!("" -as [Type])) {
     Add-Type -Path (Join-Path $PSScriptRoot JiraPS.Types.cs) -ReferencedAssemblies Microsoft.CSharp, Microsoft.PowerShell.Commands.Utility, System.Management.Automation, System.Text.RegularExpressions
@@ -9,41 +9,41 @@ if (!("" -as [Type])) {
 
 # Load Web assembly when needed
 # PowerShell Core has the assembly preloaded
-if (!("System.Web.HttpUtility" -as [Type])) {
-    Add-Type -AssemblyName "System.Web"
+if (!('System.Web.HttpUtility' -as [Type])) {
+    Add-Type -AssemblyName 'System.Web'
 }
 # Load System.Net.Http when needed
 # PowerShell Core has the assembly preloaded
-if (!("System.Net.Http.HttpRequestException" -as [Type])) {
-    Add-Type -AssemblyName "System.Net.Http"
+if (!('System.Net.Http.HttpRequestException' -as [Type])) {
+    Add-Type -AssemblyName 'System.Net.Http'
 }
-if (!("System.Net.Http" -as [Type])) {
+if (!('System.Net.Http' -as [Type])) {
     Add-Type -Assembly System.Net.Http
 }
 #endregion Dependencies
 
 #region Configuration
-$script:serverConfig = ("{0}/AtlassianPS/JiraPS/server_config" -f [Environment]::GetFolderPath('ApplicationData'))
+$script:serverConfig = ('{0}/AtlassianPS/JiraPS/server_config' -f [Environment]::GetFolderPath('ApplicationData'))
 
 if (-not (Test-Path $script:serverConfig)) {
     $null = New-Item -Path $script:serverConfig -ItemType File -Force
 }
 $script:JiraServerUrl = [Uri](Get-Content $script:serverConfig)
 
-$script:DefaultContentType = "application/json; charset=utf-8"
-$script:DefaultPageSize = 25
-$script:DefaultHeaders = @{ "Accept-Charset" = "utf-8" }
+[String]$script:DefaultContentType = 'application/json; charset=utf-8'
+[UInt32]$script:DefaultPageSize = 25
+[Hashtable]$script:DefaultHeaders = @{ 'Accept-Charset' = 'utf-8' }
 # Bug in PSv3's .Net API
 if ($PSVersionTable.PSVersion.Major -gt 3) {
-    $script:DefaultHeaders["Accept"] = "application/json"
+    $script:DefaultHeaders['Accept'] = 'application/json'
 }
 $script:PagingContainers = @(
-    "comments"
-    "dashboards"
-    "groups"
-    "issues"
-    "values"
-    "worklogs"
+    'comments'
+    'dashboards'
+    'groups'
+    'issues'
+    'values'
+    'worklogs'
 )
 $script:SessionTransformationMethod = "ConvertTo-JiraSession"
 #endregion Configuration
@@ -58,8 +58,8 @@ foreach ($file in @($PublicFunctions + $PrivateFunctions)) {
         . $file.FullName
     }
     catch {
-        $exception = ([System.ArgumentException]"Function not found")
-        $errorId = "Load.Function"
+        $exception = ([System.ArgumentException]'Function not found')
+        $errorId = 'Load.Function'
         $errorCategory = 'ObjectNotFound'
         $errorTarget = $file
         $errorItem = New-Object -TypeName System.Management.Automation.ErrorRecord $exception, $errorId, $errorCategory, $errorTarget
